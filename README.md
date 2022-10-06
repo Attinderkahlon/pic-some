f30c39435f360e82822a4f8a510c9992310b93d3
Attinder <105198941+Attinderkahlon@users.noreply.github.com>
2022-10-07 10:18:31 +1100

    Router Setup

--- README.md
+++ README.md
@@ -8,6 +8,7 @@ https://github.com/bobziroll/scrimba-react-bootcamp-images
 
 # Icon library
 https://remixicon.com/
+https://cdn.jsdelivr.net/npm/remixicon@2.3.0/fonts/remixicon.css
 
 # Libraries
 * React Router - https://reacttraining.com/react-router/web/guides/quick-start
--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import './App.css'
+import Header from './components/Header'
+import { Routes, Route } from 'react-router-dom'
+import Photos from './pages/Photos'
+import Cart from './pages/Cart'
 
 function App() {
-  return <div className='App'>react app</div>
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Photos />} />
+        <Route path='/cart' element={<Cart />} />
+      </Routes>
+    </>
+  )
 }
 
 export default App
